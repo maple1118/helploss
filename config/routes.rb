0a9cf892d42bd709739c66390b77eba96479b64c
maple1118 <french.maplexx@gmail.com>
2021-02-06 12:34:59 +0000

    管理者側　商品登録画面　商品一覧　商品編集　商品詳細

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'customer/homes#about'
+
+  devise_for :admins, controllers: {
+    sessions:      'devise/admins/sessions',
+    passwords:     'devise/admins/passwords',
+    registrations: 'devise/admins/registrations'
+  }
+  devise_for :customers, controllers: {
+    sessions:      'devise/customers/sessions',
+    passwords:     'devise/customers/passwords',
+    registrations: 'devise/customers/registrations'
+  }
+
+  namespace :admin do
+    get 'homes/top'
+    resources :products, only:[:new, :create, :index ,:show, :destroy, :edit, :update]
+  end
+
 end
